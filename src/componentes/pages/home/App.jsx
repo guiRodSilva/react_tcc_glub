1060e12587baa69bf165a62e2b4bf592734fb03e
Guilherme Rodrigues <guilherme.silva1850@etec.sp.gov.br>
2022-11-09 11:36:34 -0300

    app atualizado

--- src/componentes/pages/home/App.jsx
+++ src/componentes/pages/home/App.jsx
@@ -1,54 +1,44 @@
 import './App.css';
 
+
+
 function App() {
   return (
     <div classNameName="App">
       
-<main>
-     <section>
-    <div className="presentation-box">
-        <div className="text-presentation">
-            <h1> Gerenciamento de lixo urbano brasileiro</h1>
-            <h3>Mais que sua obrigação, reciclagem é necessidade.</h3>
-        
-        </div>
-        <img src="/Imagens/imagem-principal.png" alt="main-image" className="main-image"/> 
-    </div>
-</section>
-
-
-    <div className="title-box">
+      <main>
+    <div class="title-box">
         <h1> O que o site faz</h1>
         <p>O site de gerenciamento de lixo urbano brasileiro (GLUB) tem como propósito informar os moradores locais sobre a forma correta de descarte além de notificar e mostrar a rota do caminhão de lixo, mostrando que dia irá passar e o que irá recolher.</p>
     </div>
 
-    <div className="content-box">
-        <div className="primeira-imagem">
-            <img src="/Imagens/lixao-redonda.png" alt="main-image" className="main-image"/>
+    <div class="content-box">
+        <div class="primeira-imagem">
+            <img src="/Imagens/lixao-redonda.png" alt="main-image" class="main-image"/>
         </div>
 
-        <div className="segunda-imagem">
+        <div class="segunda-imagem">
             
-            <img src="/Imagens/praia-redondo.png" alt="main-image" className="main-image"/>
+            <img src="/Imagens/praia-redondo.png" alt="main-image" class="main-image"/>
         </div>
 
-        <div className="terceira-imagem">
+        <div class="terceira-imagem">
         
-            <img src="/Imagens/praca-redondo.png" alt="main-image" className="main-image"/>
+            <img src="/Imagens/praca-redondo.png" alt="main-image" class="main-image"/>
         </div>
     </div>
+</main>
 
-
-<section className="faq">
+<section class="faq">
     <h1> Perguntas frequentes</h1>
 
-    <div className="grid-container">
-        <div className="primeira-pergunta">
+    <div class="grid-container">
+        <div class="primeira-pergunta">
             <h3> Tem notificação de quando o caminhão passa?</h3>
             <p> Sim, tem notificação quando o caminhão estiver perto da sua casa.</p>
         </div>    
 
-        <div className="segunda-pergunta">
+        <div class="segunda-pergunta">
 
             <h3> Como funcionará o sistema de GPS?</h3>
             <p>O GPS é conectado com o caminhão de lixo mostrando sua rota atual, tendo a possibilidade de entrar e ver onde ele está na cidade.</p>
@@ -56,19 +46,18 @@ function App() {
             
         </div>
 
-        <div className="terceira-pergunta">
+        <div class="terceira-pergunta">
             <h3> A prefeitura estará com o site?</h3>
             <p> Sim, e eles irão responder suas dúvidas.</p>
         </div>
 
-        <div className="quarta-pergunta">
+        <div class="quarta-pergunta">
             <h3> Posso alterar o meu endereço a qualquer momento?</h3>
             <p>Sim, através das configurações localizadas no canto superior direito.</p>
         </div>
 
     </div>
 </section>
-</main>
 
     </div>
   );
